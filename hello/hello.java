b37baac6c3c80a0ca9ac4df77adb99c7592173f0
Quyen Ho <46450315+HOMINHQUYEN-1996-TLUS@users.noreply.github.com>
2021-08-06 23:58:17 +0700

    update

--- hello/hello.java
+++ hello/hello.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 
 public class hello {
+    public static Scanner Input = new Scanner(System.in);
+
     public static void main(String[] arg) {
-        Scanner Input = new Scanner(System.in);
-        String nhiphan = "";
+
         System.out.print("Nhap vao a : ");
         int a = Input.nextInt();
+
+        ThapphanToNhiphan(a);
+        NhiphanToThapphan();
+    }
+
+    static void ThapphanToNhiphan(int a) {
+        String nhiphan = "";
         while (a > 0) {
             if (a % 2 == 1) {
                 nhiphan = nhiphan + "1";
@@ -15,7 +23,6 @@ public class hello {
             }
             a = a / 2;
         }
-        System.out.println("a = " + a);
         /*
          * Sau khi co chuoi nhi phan. Nhung vi khi chuyen tu thap phan sang nhi phan ta
          * phai ghi tu duoi len. Nen ta dung ham reverse() co san cua JAVA de dao nguoc
@@ -23,6 +30,19 @@ public class hello {
          */
         StringBuilder ketqua = new StringBuilder(nhiphan);
         System.out.println(ketqua.reverse().toString());
-        Input.close();
+    }
+
+    static void NhiphanToThapphan() {
+        System.out.print("Nhap vao chuoi nhi phan : ");
+        String s = Input.next();
+        Double ketqua = 0.0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            /*
+             * Chuyen tu kt tu char sang so nguyen ta kep kieu dong thoi dungf bang ma ASCII
+             * de chuyen. s.charAt(i) la he 10 trong bang ma ACSII
+             */
+            ketqua = ketqua + ((int) s.charAt(i) - 48) * Math.pow(2, s.length() - i - 1);
+        }
+        System.out.println("Ket qua = " + ketqua);
     }
 }
