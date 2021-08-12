d393f55d71eb52e20bee7c33e219a3170fee23b1
Quyen Ho <46450315+HOMINHQUYEN-1996-TLUS@users.noreply.github.com>
2021-08-12 15:54:29 +0700

    Abstract_Class

---
+++ AbstractClass/index.java
@@ -0,0 +1,37 @@
+public class index {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.show();
+
+        Cat c1 = new Cat();
+        c1.show();
+
+        Animal.sound();
+    }
+}
+
+abstract class Animal {
+    Animal() {
+        System.out.println("Animal");
+    }
+
+    abstract void show();
+
+    static void sound() {
+        System.out.println("Alo Alo");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void show() {
+        System.out.println("Dog");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void show() {
+        System.out.println("Cat");
+    }
+}
--- BTVN/index.java
+++ BTVN/index.java
@@ -1,4 +1,3 @@
-
 public class index {
     public static void main(String[] args) {
         btvn a = new btvn();
